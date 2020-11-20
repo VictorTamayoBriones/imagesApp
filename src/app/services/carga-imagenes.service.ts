bbe45b317bc62e012b55ce7edcfcb53347bedea9
VictorManuelTamayo <victortamayo509@gmail.com>
2020-11-19 21:55:25 -0600

    cargarComponent

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from  '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import { FileItem } from '../models/file-item';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,12 @@ export class CargaImagenesService {
 
   constructor( private db: AngularFirestore ){ }
 
+
+  cargarImagenesFirebase( imagenes: FileItem[] ){
+
+    console.log( imagenes );
+
+  }
   private guardarImage( imagen:{ nombre: string, url: string } ){
 
     this.db.collection(`/${ this.CARPETA_IMAGENES }`)
